563ffd5075159fda7b03c2411544ff05b8015a16
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-01-22 23:44:29 +0100

    Create node.js

---
+++ node.js
@@ -0,0 +1,13 @@
+//created by abdelkarim aswan //
+
+function send() {
+localStorage.setItem("a",document.getElementById("take").value);
+}
+function check() {
+ch = document.getElementById("take").value;
+if (ch==localStorage.getItem("a")){
+console.log("you do not have any message right now");
+}else{
+document.getElementById("inner").innerText= localStorage.getItem("a");
+}
+}
