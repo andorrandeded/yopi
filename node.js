416478d71c4f6c41c010ae08ed51c702587d3183
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-01-24 14:42:55 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,13 +1,19 @@
 //created by abdelkarim aswan //
 
 function send() {
+  function populateStorage(){
 localStorage.setItem("a",document.getElementById("take").value);
 }
+
+}
 function check() {
+  
 ch = document.getElementById("take").value;
 if (ch==localStorage.getItem("a")){
 console.log("you do not have any message right now");
 }else{
+   function populateStorage(){
 document.getElementById("inner").innerText= localStorage.getItem("a");
+   }
 }
 }
